3b65cb57d6b73b26f90a2ac646760194bda6eb47
schou <pschou@users.noreply.github.com>
2023-02-24 14:12:28 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# TempFile
+
+A module to create TempFiles and then ensure they are deleted when the program is terminated.
+
+Documentation: https://pkg.go.dev/github.com/pschou/go-tempfile
---
+++ tempfile.go
@@ -0,0 +1,67 @@
+package tempfile
+
+import (
+	"math/rand"
+	"os"
+	"os/signal"
+	"path"
+	"sync"
+)
+
+// The purpose of this package is to provide temp files and then clean them up
+// when a signal terminate is received.
+
+func init() {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		for sig := range c {
+			for file, _ := range tmpFile {
+				os.Remove(file)
+			}
+		}
+	}()
+}
+
+var (
+	// Folder in which to create temp files
+	Folder = "/tmp"
+
+	// Prefix to add to the temp file names
+	Prefix = "tmp-"
+
+	tmpFile      = make(map[string]struct{})
+	tmpFileMutex sync.Mutex
+)
+
+// Remove a file and free up the resource name for future use
+func Remove(f string) {
+	tmpFileMutex.Lock()
+	defer tmpFileMutex.Unlock()
+	if _, ok := tmpFile[f]; ok {
+		delete(tmpFile, f)
+		os.Remove(f)
+	}
+}
+
+// Create a new file path for use
+func New() string {
+	tmpFileMutex.Lock()
+	defer tmpFileMutex.Unlock()
+	for {
+		test := path.Join(TempFolder, Prefix+randStringBytes(8))
+		if _, ok := tmpFile[test]; !ok {
+			tmpFile[test] = struct{}{}
+			return test
+		}
+	}
+}
+
+func randStringBytes(n int) string {
+	letterBytes := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+	}
+	return string(b)
+}
